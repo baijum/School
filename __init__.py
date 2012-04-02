0476a2038adf54713ca262946190bb5c638494b8
Baiju Muthukadan <baiju.m.mail@gmail.com>
2012-04-02 22:30:09 +0530

    implemented a simple product class

--- __init__.py
+++ __init__.py
@@ -1,2 +1,15 @@
+from OFS.Folder import Folder
+
+
+def add_school(context):
+    """Add school"""
+    context._setObject('school', School())
+
+
+class School(Folder):
+    """School"""
+    meta_type = "School"
+
+
 def initialize(context):
-    pass
+    context.registerClass(School, constructors=(add_school,))
